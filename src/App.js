b72781b37625474059b8706da620dd1399619dc5
Aaditya p. <157670638+aadim112@users.noreply.github.com>
2025-03-01 16:03:07 +0530

    Added css files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './Components/Home';
 import Account from './Components/Account';
 import Admin from './Components/Admin';
 import DoctorHome from './Components/DoctorHome'
-import AppointmentPage from './Components/Administrator';
+import AppointmentPage from './Components/AppointmentPage';
+import Administrator from './Components/Administrator';
 
 function App() {
   return (
