38f287af5721bb608de4127f1a2d08b8ddf08b25
Yash-Rajbhoj2001 <yashrajbhoj2331@gmail.com>
2025-03-01 23:00:42 +0530

    Added a Contact.js and Contact.css File

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PatientHome from './Components/PatientHome';
 import Notification from './Components/Notification';
 import HospitalHome from './Components/HospitalHome';
 import Institution from './Components/Institution';
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -25,7 +26,7 @@ function App() {
         <Link to='/Account'><i class="fa-solid fa-user" style={{color: '#ffffff',marginRight:'5px'}}></i>Account</Link>
         <Link to='/bookappointment'><i class="fa-solid fa-calendar-check" style={{color: '#ffffff',marginRight:'5px'}}></i>Book Appointment</Link>
         {/* <Link to='/Account'><i class="fa-solid fa-calendar-check" style={{color: '#ffffff',marginRight:'5px'}}></i>Search Doctor</Link> */}
-        <Link to='/Account'><i class="fa-solid fa-phone" style={{color: '#ffffff',marginRight:'5px'}}></i>Contact</Link>
+        <Link to='/Contact'><i class="fa-solid fa-phone" style={{color: '#ffffff',marginRight:'5px'}}></i>Contact</Link>
         <Link to='/Admin'><i class="fa-solid fa-user-tie" style={{color: '#ffffff',marginRight:'5px'}}></i>Admin</Link>
         <Link to='/Administator'>Administator</Link>
         <Link to='/InstituteAction'>Hospital</Link>
@@ -40,6 +41,7 @@ function App() {
       <Route path='/bookappointment' element={<AppointmentPage/>}></Route>
       {/* <Route path='/SearchDoctorPage' element={<SearchDoctorPage/>}></Route> */}
       <Route path='/Administator' element={<Administrator/>}></Route>
+      <Route path='/Contact' element={<Contact/>}></Route>
       <Route path='/InstituteAction' element={<HospitalHome/>}></Route>
       <Route path='/Institute' element={<Institution/>}></Route>
     </Routes>
